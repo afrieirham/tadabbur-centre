991bedd3a325ad066dd85a598c5a5bd356f10098
Afrie Irham <afrieirham.work@gmail.com>
2024-03-28 18:30:59 +0800

    host photos locally

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -352,24 +352,21 @@ export default function Home() {
             <div className="space-y-3">
               {[
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/1404/823a/60b6a548849e906ffd7fc068877a354f?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=q4aGYE2eucLvO4fdWd5uVaSGE0qY0nLuY471oULnR4woC5i2drM-ogP2Q~oTzJ20pYKgGyxdmmTCJ6o7mzbkSc1~dlp-QCO0TLRAzBMzNoPq6194V8cIBhxAJ341yX5wpuiKHZnjUBQOXaKTHoKSyxi1815fuxk~CQ8f~~hoUC0f9HM4tLPA10X3wIR3A3fONcmNs3DqZpK3q3Mh-Wz9mJmkgipy06hKysKubhkv12HRa9a~7NX61Vu6vnrkievpjHcxIlMGkQbc1vSpfag~Ru2jRq8XSTtnesk-awkGaQ0OJZqtjzBdk3jJZu0-VIwxWoXqCCy23~IRv8uX0poRpQ__",
+                  avatarUrl: "/avatar-1.jpeg",
                   name: "Guillermo Rauch",
                   title: "CEO, Vercel",
                   testimony:
                     "The @mintlify team absolutely nailed combining docs search and AI Q&A in a delightful experience.",
                 },
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/c4f4/b867/4b6fa8ccb330701919917101d452e1ea?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PZsTyHBEZT3q1gBE1VU8kmSQQ3Twt5JZIYjF6F1wglD~Fu~mkYy4XpNF5YJajM50xr-6hleDHvrP9SyqnnugDrpRS3jjB4rASFFuPqCIHBCVSAHuUFtc0GIM1v7YctjVlcGU4TFf0K8ZmX~n6ISM2f0wFeoJX0w8ue9y9u0wGUTtfVSIDCBX7lkWcLviB-mAqiOLzjUm1da~mIc9o2hcHttTTcnbkQDpkyTEGE7fh2VBDSxHu~r85MGeZCRvorQcmr~j9xMeKkBB~hFKCK9FMDWYje798hbduylaPtDya-sGkNyQo~269OgIsYnSktDQLD7QGsdNwOsMhpsodeYqMw__",
+                  avatarUrl: "/avatar-2.jpeg",
                   name: "Vlad Matsiiako",
                   title: "Co-founder, Infisical",
                   testimony:
                     "Satu usaha yang baik menyediakan platform untuk masyarakat memahami isi al Quran. Barakallahu fiikum buat team TC.",
                 },
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/c155/9e60/ca26672497523eb518f0c81fc4b5331a?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ib68i8qMp3GatXTDC0fYWkf3qibSTbOTKU0QSkdoV54HIjQFBXEdzf210LdljvFSd5jZXHvIxoGb7vkYkcRheAERDiUtCgADx45UeZTEL8VV~6q1jb3hBawD9TfJmqeScI5jY2ar3UuF1kK5Y3-bEapW6SFVNovq3EM5HBHXPEGhnqcnOEy09MbgPJLSu856nUpZjNPCOEj-W7RegRxE8~myUoUG~X0SP69nt6iJPnvE2AXJnACy4oh6I1Hyuw18p6Z-J0fupQTbpBzQLZbxVzME5l6uRUnfR4S8tESiiXC~uxjMxAo7qXVuJMJiRY1xdjmVprygr4-ha6tagdMuxg__",
+                  avatarUrl: "/avatar-3.jpeg",
                   name: "Maayan Salom",
                   title: "Co-founder, Elementary",
                   testimony:
@@ -396,24 +393,21 @@ export default function Home() {
             <div className="space-y-3">
               {[
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/c504/6f40/7a0ecca4239f7d65625672a6fa032486?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=paghl4Wcf5RkIbc9kkQBZKNs81JrwZtNGPZcTo66YRZMmtEKOQEiFep2ZC1D4ipBSUTxj5vutr7c~JtAhcQuwVtgUzbckUPI~oHVfFY2ISnXl40SV~KyJquXOygqAvmKMkt03EusqAevx2gVBvqBP3W9dR-QRy1aB39xm9ho0l8ZnboqpFrVZt665FkJOxx0gRioQY64bGQuE3WGSi83y18w-6bfrAZXWHpvcxzCpVG~97pikWdCeeP8zjmgH0m2ZzSgeJ~ShXWJWkAalwA4vDhdDCX33-GW9LVutSkH6e20GPvDCOdeqr32Qyw~cXv7aqnh7aZT1qeMcAfRFW~iOA__",
+                  avatarUrl: "/avatar-4.jpeg",
                   name: "Zeno Rocha",
                   title: "CEO, Resend",
                   testimony:
                     "The only one in Malaysia yang membentangkan dan memberikan ajaran terbaik  untuk memahami Al-Quran secara tadabbur yang merupakan perkara penting dalam memahami Al-Quran ! Semoga Allah berkati usaha ini.",
                 },
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/7e12/d0a0/8e7141bb44cbfb67b83244b69538f90f?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Cyx7nSd1F1jCIZD~iNFnugZO3y3JkgpsyoAH61HiXIa9KGpGSz21z-kstzXtmuq-BeJLqYbf8SvsHAebmMkko4aZ2wk751Cn1Q3ICFb7iWwMyVOlqAHkF2UrHVIjtvzDGzm~FJg7SDyPtw6ZWNbtRmPBodyLYleKWC2uEQkZMGE2M1CbOdIX2Koc3CYEqbB~8DoUYOe0yCMfeOSstMn~Ptq6Z0UpE84WRR~cAfpsGFCPF9ycu3Tl7jle1tr3YdWfuLuv~IkxRje~S~8iUvW7RgL5-ndNKkIgKfW9WUHBjeUmyyV65bWW8mRLxv-TueeIBvGEZ5VrHrGcHn2OkSZlEA__",
+                  avatarUrl: "/avatar-5.jpeg",
                   name: "Adam Carrigan",
                   title: "Co-founder, MindsDB",
                   testimony:
                     "Explanation very simple but clear. Easy to follow.",
                 },
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/b52e/9c36/d86e495ee839e4cf05ec811a43580555?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RAzI3MiL5oRVz75rmxRQ2WBmOp~2fxVpNjVmsrn3xeIe1gC8SjEL9mhTGy-AkCvqL0tNV6WZA4toi~j~VhETq-s4LBzrSUlCwiWHUA-~WwzG1OjxvBCJ4ycy4LDv9HThV1EiNHapUNEK4fbo9lHCowrfCsBJc65sOdLoig3R19IufA2IRFpWBDo-pEKDPVQRn62vgu77YKsSfVNURJFiJxGXVkGqONFrVVuUUYa8nI~n5djIXInNJZQwGLRdmBHKpuOFLcluAKAkXboEo93iXVij0npUJp5BExEkk5lk~AVJ07t-TvG95IiPIx-egMZUN2aW4HjfrD3vkHRGp2zZHQ__",
+                  avatarUrl: "/avatar-6.jpeg",
                   name: "Ashley Mulligan",
                   title: "Head of Product Engineering, Flatfile",
                   testimony:
@@ -440,24 +434,21 @@ export default function Home() {
             <div className="space-y-3">
               {[
                 {
-                  avatarUrl:
-                    "https://www.figma.com/file/29nxg1GUf0Vz4SL97ffP5c/image/f7a0739a815ab34e79a1081de8d9a402cab4675d",
+                  avatarUrl: "/avatar-7.jpeg",
                   name: "Chun Jiang",
                   title: "CEO, Monterey AI",
                   testimony:
                     "Good center for learning and understanding Quran better.",
                 },
                 {
-                  avatarUrl:
-                    "https://www.figma.com/file/29nxg1GUf0Vz4SL97ffP5c/image/901d814075d4d3902fa1617877d225040fa95352",
+                  avatarUrl: "/avatar-8.jpeg",
                   name: "Charly Poly",
                   title: "CEO, Defer",
                   testimony:
                     "MasyaAllah Alhamdulillah Syukur I was so inspired with their method & approach of explanation of The Amazing Quran since 2010 I've been following radio IKIM fm where I heard ya Ustaz Syaari Abdul Rahman every Monday 5:15pm 'Ibrah' program.",
                 },
                 {
-                  avatarUrl:
-                    "https://s3-alpha-sig.figma.com/img/27b1/531f/4b353b3410e71d55d2bdeef1e3c52b6c?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gLb6YEGW-sYlsagjuWFfm7V-J10tzZ76CGD4gHKLGkZpVGD6zJh7hcMcVDJZQ-K0aupt~XPVpu83mx2zhwAoZlsuI99rO3zwqrMoXkmTwTkoByO2t4~UH8sokBkrwFwfKzqN2wkEp~bDN3L7YQjmfsZXlOZxa6wTb5DSib0jr6ozJcda0ajjOc6R7LA0gevbad-kGfk7gbwAif9sDdILirhbuj6LPyQIk172nCGWnQJlacbPe~8KV7xTf5pfWXsSsNcMtLa2rl2ahW-tFecw9S3gvNgrabwsEYSKeVIpDM2tfdgix17TjbMbxrhw7AGtKtkKSd7Mzmyhj6X9Wh5qjQ__",
+                  avatarUrl: "/avatar-9.jpeg",
                   name: "Mark Bao",
                   title: "Co-founder, Goody",
                   testimony:
